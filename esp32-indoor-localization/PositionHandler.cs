149a3ebc119d28c2905ab58ea1b162824a5326db
g.giametta <g.giametta@reply.it>
2019-07-16 20:54:10 +0200

    getPosition() modified && RevealHiddenPosition() added

--- esp32-indoor-localization/PositionHandler.cs
+++ esp32-indoor-localization/PositionHandler.cs
@@ -40,12 +40,12 @@ namespace esp32_indoor_localization
                     // che i pacchetti siano stati ricevuti da tutti e tre le schedine (una schedina potrebbe aver catturato lo stesso pacchetto più volte!!)
 
 
-                    var validHashes = col.Find(packet => packet.timestamp >= timestamp_from && packet.timestamp <= timestamp_from + 60 && packet.mac != "hidden")
+                var validHashes = col.Find(packet => packet.timestamp >= timestamp_from && packet.timestamp <= timestamp_from + 60 && packet.mac != "hidden")
                                      .GroupBy(x => new { x.mac, x.hash, x.boardId }, (key, packet) => new { key = key, Count = packet.Count() })
                                      .GroupBy(x => new { x.key.hash, x.key.mac }, (key, packet) => new { key = key, Count = packet.Count() })
                                      .Where(grp => grp.Count == BoardLoader.Instance.NumberOfBoards)
                                      .Select(p => p.key.hash);
-
+                
 
                 //Non faccio il filter per timestamp incluso nella generazione dell'hash
                 //seleziono MAC,ID,AVGRSSI
@@ -56,7 +56,9 @@ namespace esp32_indoor_localization
                                                     (key, values) => new
                                                     {
                                                         key = key,
-                                                        AVGRSSI = values.Average(x => x.rssi)
+                                                        AVGRSSI = values.Average(x => x.rssi),
+                                                        AVGTimestamp = values.Average(x => x.timestamp)
+
                                                     }).ToList();
 
 
@@ -68,17 +70,18 @@ namespace esp32_indoor_localization
 
                 foreach (var info in deviceInfoList)
                 {
+                    string key = info.key.mac + "_" + info.AVGTimestamp.ToString();
                     string value = info.key.boardId + "_" + info.AVGRSSI.ToString();
 
                     if (!deviceInfo.ContainsKey(info.key.mac))
                     {
                         List<string> id_rssi_list = new List<string> { value };
 
-                        deviceInfo.Add(info.key.mac, id_rssi_list);
+                        deviceInfo.Add(key, id_rssi_list);
                     }
                     else
                     {
-                        deviceInfo[info.key.mac].Add(value);
+                        deviceInfo[key].Add(value);
                     }
                     
 
@@ -101,44 +104,91 @@ namespace esp32_indoor_localization
             
         }
 
-        /*public void RevealHiddenPositions(Int32 timestamp_from)
+        
+        public List<DevicePosition>  RevealHiddenPositions(Int32 timestamp_from, double threshold)
         {
-            List<Packet> revealedHiddenPacket = new List<Packet>();
-            Int32 sequenceNumber_threshold = 100;
-            int device_marker = 0;
+
+            List<DevicePosition> hiddenPositions = null;
 
             using (var db = new LiteDatabase(@"MyData.db"))
             {
                 var col = db.GetCollection<Packet>("packets");
+                                
+                var validHiddenHashes = col.Find(packet => packet.timestamp >= timestamp_from && packet.timestamp <= timestamp_from + 60 && packet.mac == "hidden")
+                                     .GroupBy(x => new { x.sequenceNumber, x.hash, x.boardId }, (key, packet) => new { key = key, Count = packet.Count() })
+                                     .GroupBy(x => new { x.key.hash, x.key.sequenceNumber }, (key, packet) => new { key = key, Count = packet.Count() })
+                                     .Where(grp => grp.Count == BoardLoader.Instance.NumberOfBoards)
+                                     .Select(p => p.key.hash);
 
-                List<Packet> hiddenPacket = col.Find(packet => packet.timestamp > timestamp_from && packet.timestamp < timestamp_from + 60 && packet.mac == "hidden").ToList(); //cast funziona??
-                foreach (Packet p1 in hiddenPacket)
+                var hiddenDeviceList = col.FindAll()
+                                      .Where(p => validHiddenHashes.Contains(p.hash))
+                                      .GroupBy(x => new { x.mac, x.hash , x.boardId },
+                                                   (key, values) => new
+                                                   {
+                                                       key = key,
+                                                       AVGRSSI = values.Average(x => x.rssi),
+                                                       AVGTimestamp = values.Average(x => x.timestamp)
+
+                                                   }).ToList();
+
+                Dictionary<string, List<string>> hiddenDeviceInfo = new Dictionary<string, List<string>>();
+
+
+                foreach (var device in hiddenDeviceList)
                 {
-                    hiddenPacket.Remove(p1);
-                    string revealedMac = "hidden_" + device_marker;
-                    p1.mac = revealedMac;
-                    revealedHiddenPacket.Add(p1);
-                    foreach (Packet p2 in hiddenPacket)
+                    string key = device.key.hash + "_" + device.AVGTimestamp.ToString();
+                    string value = device.key.boardId + "_" + device.AVGRSSI.ToString();
+
+                    if (!hiddenDeviceInfo.ContainsKey(device.key.hash))
                     {
-                        if (p1.sequenceNumber - p2.sequenceNumber < sequenceNumber_threshold)
+                        List<string> id_rssi_list = new List<string> { value };
+
+                        hiddenDeviceInfo.Add(device.key.hash, id_rssi_list);
+                    }
+                    else
+                    {
+                        hiddenDeviceInfo[device.key.hash].Add(value);
+                    }
+
+
+                }
+
+                hiddenPositions = Trilateration(hiddenDeviceInfo);
+                List<DevicePosition> hiddenDevicePositions =  null;
+
+                foreach (var position1 in hiddenPositions)
+                {
+                    hiddenDevicePositions.Add(position1);
+                    hiddenPositions.Remove(position1);
+
+                    foreach(var position2 in hiddenPositions)
+                    {
+                        if(EuclideanDistance(position1.X,position1.Y,position2.X,position2.Y,threshold))
                         {
-                            hiddenPacket.Remove(p2);
-                            p2.mac = revealedMac;
-                            revealedHiddenPacket.Add(p2);
+                            hiddenPositions.Remove(position2);
                         }
                     }
-                    device_marker++;
+                }
+
+                if(hiddenDevicePositions.Count()!=0)
+                {
+                    var positionCollection = db.GetCollection<DevicePosition>("positions");
+
+                    foreach(var position in hiddenDevicePositions)
+                    {
+                        positionCollection.Insert(position);
+                    }
+
+                    return hiddenDevicePositions;
                 }
 
 
-
-
-
-
             }
-        }*/
 
+            return null;
+        }
 
+    
         private List<DevicePosition> Trilateration(Dictionary<string, List<string>> deviceInfo)
         {
             
@@ -152,12 +202,14 @@ namespace esp32_indoor_localization
 
             foreach (var device in deviceInfo)
             {
-                //log.Info("Sto processando il MAC: " + device.Key);
+                String mac = device.Key.Split('_')[0];
+                Int32 timestamp = Int32.Parse(device.Key.Split('_')[1]);
+
+                //log.Info("Sto processando il MAC: " + mac);
                 foreach (var info in device.Value)
                 {
 
                     String id = info.Split('_')[0];
-                    Debug.WriteLine(info.Split('_')[1].Replace(',', '.'));
                     Double rssi = Double.Parse(info.Split('_')[1].Replace(',', '.'));
                     Double rssi_to_meters = ConvertRSSI(rssi);
                     Console.WriteLine("metri: " + rssi_to_meters);
@@ -212,11 +264,19 @@ namespace esp32_indoor_localization
                 Double x = (x_min_max + x_max_min) / 2;
                 Double y = (y_min_max + y_max_min) / 2;
                 //log.Info("MAC " + device.Key + " con coordinate (" + x + "," + y + ") aggiunto");
-                Int32 now = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
-                positions.Add(new DevicePosition(device.Key, x, y, now));
+                //Int32 now = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+                if (mac == "hidden") mac = timestamp.GetHashCode().ToString();
+                positions.Add(new DevicePosition(mac, x, y, timestamp));
             }
 
             return positions;
+        }
+
+        private Boolean EuclideanDistance(double x1, double y1, double x2, double y2, double threshold)
+        {
+            return (Math.Sqrt(((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))) <= threshold) ? true : false;
+
+            
         }
 
 
