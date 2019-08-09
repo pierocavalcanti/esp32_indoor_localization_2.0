773c34fb6fe410d540f4ce2493f8068e0ffec0bc
Pdipedro25 <34885983+Pdipedro25@users.noreply.github.com>
2019-08-09 14:48:08 +0200

    PasswordForm added

---
+++ esp32-indoor-localization/PasswordForm.cs
@@ -0,0 +1,90 @@
+﻿namespace esp32_indoor_localization
+{
+using System.Windows.Forms;
+using System.Drawing;
+class PasswordForm
+{
+    private Form frm;
+    public string Show(string prompt, string title)
+    {
+        frm = new Form();
+        FlowLayoutPanel FL = new FlowLayoutPanel();
+        Label lbl = new Label();
+        TextBox txt = new TextBox();
+        Button ok = new Button();
+        Button cancel = new Button();
+
+        frm.Font = new Font("Calibri", 9, FontStyle.Bold);
+        frm.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+        frm.StartPosition = FormStartPosition.CenterScreen;
+        frm.Width = 200;
+        frm.Height = 150;
+
+        frm.Text = title;
+        lbl.Text = prompt;
+        ok.Text = "Ok";
+        cancel.Text = "Cancel";
+        txt.PasswordChar = '*';
+
+        ok.FlatStyle = FlatStyle.Flat;
+        ok.BackColor = SystemColors.ButtonShadow;
+        ok.ForeColor = SystemColors.ButtonHighlight;
+        ok.Cursor = Cursors.Hand;
+
+        cancel.FlatStyle = FlatStyle.Flat;
+        cancel.BackColor = SystemColors.ButtonShadow;
+        cancel.ForeColor = SystemColors.ButtonHighlight;
+        cancel.Cursor = Cursors.Hand;
+
+        FL.Left = 0;
+        FL.Top = 0;
+        FL.Width = frm.Width;
+        FL.Height = frm.Height;
+        FL.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top;
+        FL.Padding = new Padding(5);
+        FL.FlowDirection = FlowDirection.TopDown;
+
+        ok.Width = FL.Width - 35;
+        txt.Width = ok.Width;
+        cancel.Width = ok.Width;
+        lbl.Width = ok.Width;
+
+        ok.Click += new System.EventHandler(okClick);
+        cancel.Click += new System.EventHandler(cancelClick);
+        txt.KeyPress += new KeyPressEventHandler(txtEnter);
+
+        FL.Controls.Add(lbl);
+        FL.Controls.Add(txt);
+        FL.Controls.Add(ok);
+        FL.Controls.Add(cancel);
+        frm.Controls.Add(FL);
+
+        frm.ShowDialog();
+        DialogResult DR = frm.DialogResult;
+        frm.Dispose();
+        frm = null;
+        if (DR == DialogResult.OK)
+        {
+            return txt.Text;
+        }
+        else
+        {
+            return "";
+        }
+    }
+    private void okClick(object sender, System.EventArgs e)
+    {
+        frm.DialogResult = DialogResult.OK;
+        frm.Close();
+    }
+    private void cancelClick(object sender, System.EventArgs e)
+    {
+        frm.DialogResult = DialogResult.Cancel;
+        frm.Close();
+    }
+    private void txtEnter(object sender, KeyPressEventArgs e)
+    {
+        if (e.KeyChar == 13) { okClick(null, null); }
+    }
+}
+}
